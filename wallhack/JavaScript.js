5e3d420b4f9d472d213981366c2a8db7e2d2db58
hist-site <63604237+hist-site@users.noreply.github.com>
2020-12-16 05:06:43 +0200

    init commit

---
+++ wallhack/JavaScript.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.menu-btn').on('click', function(e) {
+  	e.preventDefault();
+  	$(this).toggleClass('menu-btn_active');
+  	$('.menu').toggleClass('menu_active');
+	});
+});
+
+
+
+
+
