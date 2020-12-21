8f96c8b19a1ee94d39d289e577784671e4896742
hist-site <63604237+hist-site@users.noreply.github.com>
2020-12-21 05:37:06 +0200

    init commit

--- wallhack/JavaScript.js
+++ wallhack/JavaScript.js
@@ -1,7 +1,19 @@
 $(function(){
 	$('.menu-btn').on('click', function(e) {
-  	e.preventDefault();
+  		e.preventDefault();
   	$(this).toggleClass('menu-btn_active');
   	$('.menu').toggleClass('menu_active');
+	});
+
+	$('.menu-btn-on').on('click', function(e) {
+		e.preventDefault();
+	$('.menu-info').toggleClass('menu-info_active');
+	$('.wrapper').toggleClass('wrapper_active');
+	});
+
+	$('.menu-btn-off').on('click', function(e) {
+		e.preventDefault();
+	$('.menu-info').toggleClass('menu-info_active');
+	$('.wrapper').toggleClass('wrapper_active');
 	});
 });
