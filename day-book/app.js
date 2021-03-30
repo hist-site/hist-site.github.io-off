837f502535428311da74bb7f8b9f080315089c27
hist-site <63604237+hist-site@users.noreply.github.com>
2021-03-31 02:49:18 +0300

    init commit

--- day-book/app.js
+++ day-book/app.js
@@ -1,10 +1,30 @@
-$(function() {
-    /* Slider */
-    $("[data-slider]").slick({
-        infinite: true,
-        fade: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
+const date = new Date(2021,1,2,0);
 
+const renderCalendar = () => {
+
+  const monthDays = document.querySelector(".days");
+
+  const days = [
+    	"Понеділок",
+    	"Вівторок",
+   		"Середа",
+    	"Четвер",
+    	"П'ятниця",
+    	"Субота",
+    	"Неділя",
+  ];
+
+  document.querySelector(".date h1").innerHTML = days[date.getDay()];
+
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setDate(date.getDay() - 1);
+  renderCalendar();
 });
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setDate(date.getDay() + 1);
+  renderCalendar();
+});
+renderCalendar();
